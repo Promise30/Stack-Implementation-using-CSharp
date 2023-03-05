fe2ebe017cf64986a1655f4dd77fb0753ae66c79
Promise30 <promiseadeagbo@gmail.com>
2023-03-05 17:57:24 +0100

    Updated Stack class containing necessary methods

---
+++ Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation_using_CSharp
+{
+    public class Stack
+    {
+        List<object> list = new List<object>();
+        public void Push(object obj)
+        {
+            if(obj == null)
+            {
+                throw new InvalidOperationException("Object of type 'null' cannot be passed in to this method.");
+            }
+            list.Add(obj);
+        }
+        public object Pop() 
+        {
+            if(list.Count == 0)
+                throw new InvalidOperationException("Cannot pop an element off an empty stack.");
+            
+            var item = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+
+            return item;
+        }
+        public void Clear() 
+        { 
+            list.Clear();
+        }
+
+
+    }
+}
