8c85eb8cd25375b30e3b44b4747c94a7bed18dd4
Promise30 <promiseadeagbo@gmail.com>
2023-03-05 17:57:45 +0100

    Updated file containing implementation of the Stack class

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_Implementation_using_CSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack= new Stack();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            Console.ReadKey();
+        }
+    }
+}
